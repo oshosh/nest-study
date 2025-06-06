0c76e8c62e57bdedd2421aaf06dc304a2323bb4b
oshosh <dhtpgus7@gmail.com>
2025-06-06 15:39:57 +0900

    feat: One to One CRUD

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MovieDetail } from './entity/movie-detail.entity';
 import { Movie } from './entity/movie.entity';
 import { MovieController } from './movie.controller';
 import { MovieService } from './movie.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Movie])],
+  imports: [TypeOrmModule.forFeature([Movie, MovieDetail])],
   controllers: [MovieController],
   providers: [MovieService],
 })
--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Like, Repository } from 'typeorm';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { MovieDetail } from './entity/movie-detail.entity';
 import { Movie } from './entity/movie.entity';
 
 @Injectable()
@@ -10,6 +11,8 @@ export class MovieService {
   constructor(
     @InjectRepository(Movie)
     private readonly movieRepository: Repository<Movie>,
+    @InjectRepository(MovieDetail)
+    private readonly movieDetailRepository: Repository<MovieDetail>,
   ) {}
 
   async getManyMovies(title?: string) {
@@ -26,7 +29,10 @@ export class MovieService {
   }
 
   async getMovieById(id: number) {
-    const movie = await this.movieRepository.findOne({ where: { id } });
+    const movie = await this.movieRepository.findOne({
+      where: { id },
+      relations: ['detail'],
+    });
 
     if (!movie) {
       throw new NotFoundException('Movie not found');
@@ -36,37 +42,63 @@ export class MovieService {
   }
 
   async createMovie(createMovieDto: CreateMovieDto) {
-    const movie = await this.movieRepository.save(createMovieDto);
+    const movie = await this.movieRepository.save({
+      title: createMovieDto.title,
+      genre: createMovieDto.genre,
+      detail: {
+        detail: createMovieDto.detail,
+      },
+    });
     return movie;
   }
 
   async updateMovie(id: number, updateMovieDto: UpdateMovieDto) {
-    const movie = await this.movieRepository.findOne({ where: { id } });
+    const movie = await this.movieRepository.findOne({
+      where: { id },
+      relations: ['detail'],
+    });
 
     if (!movie) {
       throw new NotFoundException('Movie not found');
     }
 
+    const { detail, ...movieRest } = updateMovieDto;
+
     await this.movieRepository.update(
       {
         id,
       },
-      updateMovieDto,
+      movieRest,
     );
 
-    const newMovie = await this.movieRepository.findOne({ where: { id } });
+    // detail 이 존재한다면?
+    if (detail) {
+      await this.movieDetailRepository.update(
+        { id: movie.detail.id },
+        { detail },
+      );
+    }
+
+    const newMovie = await this.movieRepository.findOne({
+      where: { id },
+      relations: ['detail'],
+    });
 
     return newMovie;
   }
 
   async deleteMovie(id: number) {
-    const movie = await this.movieRepository.findOne({ where: { id } });
+    const movie = await this.movieRepository.findOne({
+      where: { id },
+      relations: ['detail'],
+    });
 
     if (!movie) {
       throw new NotFoundException('Movie not found');
     }
 
     await this.movieRepository.delete(id);
+    await this.movieDetailRepository.delete(movie.detail.id);
 
     return id;
   }
