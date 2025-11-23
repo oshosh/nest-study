7cefe1ed5de2ae51e05b57195de1382eec261ff5
oshosh <dhtpgus7@gmail.com>
2025-11-23 16:31:25 +0900

    feat: 다중 파일 업로드

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -10,10 +10,10 @@ import {
   Post,
   Query,
   Req,
-  UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import { Public } from 'src/auth/decorator/public.decorator';
 import { RBAC } from 'src/auth/decorator/rbac.decorator';
 import { TransactionInterceptor } from 'src/common/interceptor/transaction.interceptor';
@@ -56,13 +56,16 @@ export class MovieController {
   @Post()
   @RBAC(Role.admin)
   @UseInterceptors(TransactionInterceptor)
-  @UseInterceptors(FileInterceptor('movie'))
+  @UseInterceptors(FilesInterceptor('movies'))
   postMovie(
     @Body() body: CreateMovieDto,
     @Req() req: Request & { queryRunner: QueryRunner },
-    @UploadedFile() file: Express.Multer.File,
+    @UploadedFiles() files: Express.Multer.File[],
   ) {
-    console.log(file);
+    console.log('--------------------------------');
+    console.log(files);
+    console.log('--------------------------------');
+
     return this.movieService.create(body, req.queryRunner);
   }
 
